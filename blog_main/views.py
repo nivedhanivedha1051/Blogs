0277adf97d79ff6eb320dc0733ce947d3d023493
Nivedha V <nivedhanivedha1051@gmail.com>
2026-08-16 01:24:47 -0700

    category crud functionality

--- blog_main/views.py
+++ blog_main/views.py
@@ -63,7 +63,7 @@ def login(request):
 
             if user is not None:
                 auth.login(request, user)
-                return redirect('home')
+                return redirect('dashboard')
 
     else:
         form = AuthenticationForm()
